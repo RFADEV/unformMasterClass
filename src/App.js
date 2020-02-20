c53d4a30c063bfc56ab9e28d14f94ac5410752de
RFADEV <filgueiras.rfa@gmail.com>
2020-02-20 01:50:29 -0300

    feito ate a validacao dos campos com Yup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useRef } from 'react';
+import { Form } from '@unform/web';
+import { Scope } from '@unform/core';
+import * as Yup from 'yup';
 import './App.css';
 
+import Input from './components/Form/Input';
+
+const initialData = {
+  email: 'rfadev@renan.com.br',
+  address: {
+    city: 'Muriae'
+  }
+}
+
+// const user = {
+//   name: "Diego",
+//   address: {
+//     street: 'Rua Teste',
+//     number: 123,
+//   }
+// }
+
 function App() {
+  const formRef = useRef(null);
+
+ async function handleSubmit(data, { reset }) {
+    try {
+
+      const schema = Yup.object().shape({
+        name: Yup.string().required('O nome é obrigatório'),
+        email: Yup.string().email('Digite Um Email Válido').required('O e-mail é obrigatório'),
+        address: Yup.object().shape({
+          city: Yup.string().min(3, 'No mínimo 3 Caracteres').required('A Cidade é obrigatória'),
+        })
+      });
+
+      if (data.name === "") {
+        formRef.current.setErrors({
+          name: 'O nome é obrigatorio',
+          address: {
+            city: 'A Cidade é obritagória'
+          }
+        });
+      }
+
+      await schema.validate(data, {
+        abortEarly:false,
+      })
+
+      console.log(data);
+
+      formRef.current.setErrors({});
+
+      reset();
+    }catch(err){
+      if (err instanceof Yup.ValidationError){
+        const errorMessage = {
+          name:"Nome Obrigatório"
+        };
+
+        err.inner.forEach(error => {
+          errorMessage[error.path] = error.message;
+        });
+
+        formRef.current.setErrors(errorMessage);
+
+      }
+    }
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World</h1>
+      <Form ref={formRef} initialData={initialData} onSubmit={handleSubmit}>
+        <Input name="name" />
+        <Input type="email" name="email" />
+        <Scope path="address">
+          <Input name="street" />
+          <Input name="neighborhood" />
+          <Input name="city" />
+          <Input name="state" />
+          <Input name="number" />
+        </Scope>
+
+
+        <button type="submit">Enviar</button>
+      </Form>
+
     </div>
   );
 }
